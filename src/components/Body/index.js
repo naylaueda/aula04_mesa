23825d48a94800b3dc61cf0e9fdadade323f718b
naylaueda <nayla_ueda@yahoo.com.br>
2022-03-06 15:03:21 -0300

    add files

---
+++ src/components/Body/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+import foto from '../../assets/homem-feliz-dinheiro.jpg'
+
+export default function Body() {
+    return (
+ <img src={foto} alt='foto' id='foto'/>
+    )
+
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+export default function Header(){
+    return (
+        <header className = 'header_home'>
+            <h1 id='title'>Aula 04 - Mesa de Trabalho</h1>
+        </header>
+    )
+}
